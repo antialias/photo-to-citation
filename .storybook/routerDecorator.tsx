684a0ed1cb909a5b6e9db80ef9983a57c68ef02f
Thomas Hallock <antialias@users.noreply.github.com>
2025-06-14 17:46:52 -0500

    fix: provide router contexts for storybook

--- .storybook/routerDecorator.tsx
+++ .storybook/routerDecorator.tsx
@@ -1,5 +1,10 @@
 import type { Decorator } from "@storybook/react";
 import { AppRouterContext } from "next/dist/shared/lib/app-router-context.shared-runtime";
+import {
+  PathParamsContext,
+  PathnameContext,
+  SearchParamsContext,
+} from "next/dist/shared/lib/hooks-client-context.shared-runtime";
 
 const router = {
   back: () => {},
@@ -12,6 +17,12 @@ const router = {
 
 export const withRouter: Decorator = (Story) => (
   <AppRouterContext.Provider value={router}>
-    {Story()}
+    <PathnameContext.Provider value="/">
+      <SearchParamsContext.Provider value={new URLSearchParams()}>
+        <PathParamsContext.Provider value={{}}>
+          {Story()}
+        </PathParamsContext.Provider>
+      </SearchParamsContext.Provider>
+    </PathnameContext.Provider>
   </AppRouterContext.Provider>
 );
