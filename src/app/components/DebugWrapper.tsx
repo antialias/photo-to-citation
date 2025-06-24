1c129ca43d18bc67c88b8d6a40762028141f8a7e
Thomas Hallock <antialias@users.noreply.github.com>
2025-06-24 13:12:13 -0500

    feat: make debug tooltip interactive and wrap

--- src/app/components/DebugWrapper.tsx
+++ src/app/components/DebugWrapper.tsx
@@ -64,11 +64,12 @@ export default function DebugWrapper({
           >
             Copy
           </button>
-          <pre className="pt-4">{tokens}</pre>
+          <pre className="pt-4 whitespace-pre-wrap break-words">{tokens}</pre>
         </div>
       }
       open={show}
       onOpenChange={setOpen}
+      interactive
     >
       <div
         onMouseEnter={() => {
