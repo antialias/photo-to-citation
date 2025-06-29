34e847e650524e478f349b00d418af01b6f188d9
Thomas Hallock <antialias@users.noreply.github.com>
2025-06-29 11:56:17 -0500

    fix: include public env on server

--- src/publicEnv.ts
+++ src/publicEnv.ts
@@ -5,6 +5,14 @@ export interface PublicEnv {
 }
 
 export function getPublicEnv(): PublicEnv {
-  if (typeof window === "undefined") return {};
+  if (typeof window === "undefined") {
+    return {
+      NEXT_PUBLIC_GOOGLE_MAPS_API_KEY:
+        process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
+      NEXT_PUBLIC_BROWSER_DEBUG:
+        process.env.NEXT_PUBLIC_BROWSER_DEBUG === "true",
+      NEXT_PUBLIC_BASE_PATH: process.env.NEXT_PUBLIC_BASE_PATH,
+    };
+  }
   return (window as unknown as { PUBLIC_ENV?: PublicEnv }).PUBLIC_ENV ?? {};
 }
