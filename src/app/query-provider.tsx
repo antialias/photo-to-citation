f002aa54f727933fc0d3d189455138fbf4e58a8f
Thomas Hallock <antialias@users.noreply.github.com>
2025-06-29 09:19:52 -0500

    fix: initialize react query client inside provider

--- src/app/query-provider.tsx
+++ src/app/query-provider.tsx
@@ -2,13 +2,15 @@
 
 import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import queryClient from "./queryClient";
+import { useState } from "react";
+import { makeQueryClient } from "./queryClient";
 
 export default function QueryProvider({
   children,
 }: { children: React.ReactNode }) {
+  const [client] = useState(makeQueryClient);
   return (
-    <QueryClientProvider client={queryClient}>
+    <QueryClientProvider client={client}>
       {children}
       {process.env.NODE_ENV === "development" && (
         <ReactQueryDevtools initialIsOpen={false} />
