7d5a32e91f3a3374564240507f9fa7d4ad3fb730
Thomas Hallock <thomas@evolutioniq.com>
2025-06-30 20:43:19 -0500

    setting source maps in npm script too

--- next.config.ts
+++ next.config.ts
@@ -21,6 +21,12 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  webpack: (config, {isServer}) => {
+    if (isServer) {
+      config.devtool = 'source-map'
+    }
+    return config
+  },
 };
 
 export default nextConfig;
